e3a836f4af03adc39f2a47826bdd2fc622549b93
Priyanka T <priyankataug98@gmail.com>
2025-10-16 16:59:25 +0000

    add this section colrowhite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,16 +60,18 @@ export default function Page() {
             <Building className="w-12 h-12 mb-2 text-gray-600" />
             <p className="font-bold text-lg">Real estate</p>
           </Card>
-          <Card className="col-span-1 bg-[#EAF0EF] p-6 flex flex-col justify-center items-center rounded-2xl text-center">
+          <Card className="col-span-2 bg-[#EAF0EF] p-6 flex flex-col justify-center items-center rounded-2xl text-center">
             <Warehouse className="w-12 h-12 mb-2 text-gray-600" />
             <p className="font-bold text-lg">Rentals</p>
           </Card>
-          <Card className="col-span-1 bg-[#0d1a1a] text-white p-6 flex flex-col justify-center items-center rounded-2xl text-center">
-            <LandPlot className="w-12 h-12 mb-2" />
-            <p className="font-bold text-lg">Land</p>
-          </Card>
         </div>
       </main>
+
+      <section className="bg-white py-20">
+        <div className="container mx-auto text-center">
+          <h2 className="text-3xl font-bold">People Soft</h2>
+        </div>
+      </section>
     </div>
   );
 }
